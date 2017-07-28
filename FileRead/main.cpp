72ad5f2e5bb1693898e76c607b2a8eab944c2573
chudongfang <1149669942@qq.com>
2017-07-28 18:00:42 +0800

    FileReader

---
+++ FileRead/main.cpp
@@ -0,0 +1,34 @@
+#include "FileUtil.h"
+
+#include <stdio.h>
+#define __STDC_FORMAT_MACROS
+#include <inttypes.h>
+#include <iostream>
+
+using namespace muduo;
+
+int main()
+{
+    string result;
+    int64_t size = 0;
+    FileUtil::readFile("./Makefile",1024,&result,&size);
+    std::cout << result<<std::endl;
+    std::cout << size  <<std::endl;
+    int err = FileUtil::readFile("/proc/self", 1024, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/proc/self", 1024, &result, NULL);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/proc/self/cmdline", 1024, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/dev/null", 1024, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/dev/zero", 1024, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/notexist", 1024, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/dev/zero", 102400, &result, &size);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+    err = FileUtil::readFile("/dev/zero", 102400, &result, NULL);
+    printf("%d %zd %" PRIu64 "\n", err, result.size(), size);
+}
+
